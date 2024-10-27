149ae00a6926f4732b410bd5ae8cb0fdfa95b469
Chairul Amri <chairoelamri2610@gmail.com>
2024-10-27 14:44:11 +0700

    update splash screen

--- app/src/main/java/com/learn/nasho/ui/views/SplashScreenActivity.kt
+++ app/src/main/java/com/learn/nasho/ui/views/SplashScreenActivity.kt
@@ -1,21 +1,62 @@
 package com.learn.nasho.ui.views
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.learn.nasho.R
+import com.learn.nasho.databinding.ActivitySplashScreenBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    private val activityScope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        displayAppVersion()
+
+        activityScope.launch(Dispatchers.Main) {
+            delay(2000)
+            val intent = Intent(this@SplashScreenActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun displayAppVersion() {
+        binding.apply {
+            try {
+                val pInfo: PackageInfo = packageManager.getPackageInfo(packageName, 0)
+                tvAppVersion.text = buildString {
+                    append("V ")
+                    append(pInfo.versionName)
+                }
+            } catch (e: PackageManager.NameNotFoundException) {
+                e.printStackTrace()
+            }
         }
     }
 }
